964780692fde1f77c353d133fbb76ae2436dbb26
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-06-09 12:50:39 +0900

    make DownloaderScene task list view

--- src/dogaas/downloader.py
+++ src/dogaas/downloader.py
@@ -1,11 +1,33 @@
 from dataclasses import dataclass
+from typing import Callable, Optional
 import abc
-from typing import Optional
+import re
+
+
+def is_url(url: str, raise_if_not=False) -> bool:
+    if re.match(r"^https?://", url):
+        return True
+    else:
+        if raise_if_not:
+            raise ValueError(f"`{url}` is not a valid URL")
+        return False
 
 
 @dataclass
 class DownloaderTask:
-    url: str
+    _url: str
+
+    @property
+    def url(self) -> str:
+        is_url(self._url, raise_if_not=True)
+        return self._url
+
+    def __post_init__(self):
+        is_url(self.url, raise_if_not=True)
+
+    def rewrite_url(self, new_url: str):
+        if is_url(new_url, raise_if_not=True):
+            self._url = new_url
 
 
 Tasks = dict[str, DownloaderTask]
@@ -22,10 +44,22 @@ class TaskDatabaseInterface(metaclass=abc.ABCMeta):
 
 
 class TaskManager(TaskDatabaseInterface):
-    def __init__(self, **tasks: dict[str, DownloaderTask]):
+    def __init__(
+        self,
+        tasks: Optional[dict[str, DownloaderTask]] = None,
+        on_add: Optional[Callable[..., None]] = None,
+        on_remove: Optional[Callable[..., None]] = None,
+        on_rename: Optional[Callable[..., None]] = None,
+        on_download: Optional[Callable[..., None]] = None,
+    ):
         self.tasks: Tasks = {}
-        for name, task in tasks.items():
-            self.add_task(name, task)
+        self.on_add = on_add
+        self.on_remove = on_remove
+        self.on_rename = on_rename
+        self.on_download = on_download
+        if tasks:
+            for name, task in tasks.items():
+                self.add_task(name, task)
 
     def add_task(
         self,
@@ -34,30 +68,37 @@ class TaskManager(TaskDatabaseInterface):
     ):
         if isinstance(task, DownloaderTask):
             self.tasks[name] = task
+            if self.on_add:
+                self.on_add()
         else:
             raise TypeError("`task` must be `DownloaderTask`")
 
     def rename_task(self, task_name: str, new_name: str):
         if isinstance(task_name, str):
             self.tasks[new_name] = self.tasks.pop(task_name)
+            if self.on_rename:
+                self.on_rename()
         else:
             raise TypeError("`task_name` must be a `str`")
 
     def remove_task(self, task_name: str):
         if isinstance(task_name, str):
             self.tasks.pop(task_name)
+            if self.on_remove:
+                self.on_remove()
         else:
             raise TypeError("`task_name` must be `str`")
 
     def download(self, task_name: str):
         if isinstance(task_name, str):
-            raise NotImplementedError
+            if self.on_download:
+                self.on_download()
         else:
             raise TypeError("`task_name` must be `str`")
 
 
 class Downloader:
-    def __init__(self, task_manager: Optional[None] = None):
+    def __init__(self, task_manager: Optional[TaskManager] = None):
         """
         Args:
             task_manager (Optional[None]):
--- src/gui_app.py
+++ src/gui_app.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import sys
 import json
+from typing import Callable
 
 import flet as ft
 
-from dogaas.downloader import Downloader
+from dogaas.downloader import TaskManager, DownloaderTask, is_url
 
 THIS_SCRIPT_DIR = Path(sys.argv[0]).parent.absolute()
 CONFIG_FILENAME = "config.json"
@@ -19,46 +20,148 @@ with open(
 ) as f:
     i18ntexts: dict[str, str] = json.load(f)
 
-downloader = Downloader()
-
-
-class TasksView(ft.UserControl):
-    def build(self):
-        tasks_view = ft.ListView(expand=1)
-        return tasks_view
-
 
 class TaskDisplay(ft.UserControl):
-    def __init__(self, task_name: str):
-        self.task_name = task_name
+    def __init__(
+        self,
+        downloader_task: DownloaderTask,
+        task_name: str,
+        task_manager: TaskManager,
+        delete_task_func: Callable,
+    ):
+        super().__init__()
+        if isinstance(downloader_task, DownloaderTask):
+            self.task = downloader_task
+        else:
+            raise TypeError("`downloader_task` must be `DownloaderTask`")
+        if isinstance(task_name, str):
+            self.task_name = task_name
+        else:
+            raise TypeError("`task_name` must be `str`")
+        if isinstance(task_manager, TaskManager):
+            self.task_manager = task_manager
+        else:
+            raise TypeError("`task_manager` must be `TaskManager`")
+        self.delete_task_func = delete_task_func
 
     def build(self):
-        remove_btn = ft.IconButton(ft.icons.DELETE)
-        url_display = ft.TextField(value=self.task_name)
-        return ft.Row([url_display, remove_btn])
+        remove_btn = ft.IconButton(
+            ft.icons.DELETE, on_click=lambda e: self.remove_task()
+        )
+        self.task_name_display = ft.TextField(
+            value=self.task_name, on_submit=lambda e: self.rename_task()
+        )
+        self.url_display = ft.TextField(
+            value=self.task.url, on_submit=lambda e: self.rewrite_task_url()
+        )
+        return ft.Container(
+            ft.Row(
+                [
+                    ft.Column([self.task_name_display, self.url_display], expand=1),
+                    remove_btn,
+                ],
+                expand=1,
+            ),
+            padding=2,
+            border_radius=3,
+            border=ft.Border(*[ft.BorderSide(1, ft.colors.TEAL)] * 4),
+        )
+
+    def rename_task(self):
+        if self.task_name_display.value:
+            self.task_manager.rename_task(self.task_name, self.task_name_display.value)
+            self.task_name_display.error_text = None
+            self.task_name_display.update()
+
+        else:
+            self.task_name_display.error_text = i18ntexts["input_dl_task_name"]
+            self.task_name_display.update()
+
+    def remove_task(self):
+        self.task_manager.remove_task(self.task_name)
+        self.delete_task_func(self)
+
+    def rewrite_task_url(self):
+        if is_url(self.url_display.value):
+            self.task_manager.tasks[self.task_name].rewrite_url(self.url_display.value)
+            self.task_name_display.error_text = None
+            self.task_name_display.update()
+        else:
+            self.task_name_display.error_text = i18ntexts["input_dl_url"]
+            self.task_name_display.update()
 
 
 class DownloaderScene(ft.UserControl):
     def build(self):
+        self.dltask_manager = TaskManager()
+        self.tasks_view = ft.Column(expand=1, spacing=4, scroll=ft.ScrollMode.ALWAYS)
         add_task_btn = ft.ElevatedButton(
             text=i18ntexts["add_task"],
             icon=ft.icons.ADD,
-            expand=1
+            expand=1,
+            on_click=lambda e: self.add_task(),
         )
-        new_task_name_textfield = ft.TextField()
-        new_task_url_textfield = ft.TextField()
-        tasks_view = TasksView()
+        self.new_task_name_textfield = ft.TextField(
+            hint_text=i18ntexts["input_dl_task_name"],
+        )
+        self.new_task_url_textfield = ft.TextField(hint_text=i18ntexts["input_dl_url"])
         return ft.Container(
             ft.Column(
                 [
-                    new_task_name_textfield,
-                    new_task_url_textfield,
+                    self.new_task_name_textfield,
+                    self.new_task_url_textfield,
                     ft.Row([add_task_btn]),
-                    tasks_view
-                ]
+                    self.tasks_view,
+                ],
+                expand=1,
             ),
             padding=10,
+            expand=1,
         )
+
+    def add_task(self):
+        invalid_task = False
+        if not (task_name := self.new_task_name_textfield.value):
+            self.new_task_name_textfield.error_text = i18ntexts["input_dl_task_name"]
+            self.new_task_name_textfield.update()
+            invalid_task = True
+        elif self.dltask_manager.tasks.get(task_name):
+            self.new_task_name_textfield.error_text = i18ntexts["duplicate_task_name"]
+            self.new_task_name_textfield.update()
+            invalid_task = True
+        if not is_url(task_url := self.new_task_url_textfield.value):
+            self.new_task_url_textfield.error_text = i18ntexts["input_dl_url"]
+            self.new_task_url_textfield.update()
+            invalid_task = True
+        if invalid_task:
+            return
+        else:
+            new_task = DownloaderTask(task_url)
+            self.dltask_manager.add_task(name=task_name, task=new_task)
+            self._add_taskdisplay(
+                TaskDisplay(
+                    new_task,
+                    task_name=task_name,
+                    task_manager=self.dltask_manager,
+                    delete_task_func=self._delete_task,
+                )
+            )
+            self.new_task_name_textfield.error_text = None
+            self.new_task_name_textfield.update()
+            self.new_task_url_textfield.error_text = None
+            self.new_task_url_textfield.update()
+
+    def _add_taskdisplay(self, taskdisplay: TaskDisplay):
+        if isinstance(taskdisplay, TaskDisplay):
+            self.tasks_view.controls.append(taskdisplay)
+            self.tasks_view.update()
+        else:
+            raise TypeError("`taskdisplay` must be `TaskDisplay`")
+
+    def _delete_task(self, taskdisplay: TaskDisplay):
+        if isinstance(taskdisplay, TaskDisplay):
+            self.tasks_view.controls.remove(taskdisplay)
+            self.tasks_view.update()
 
 
 class SettingsScene(ft.UserControl):
